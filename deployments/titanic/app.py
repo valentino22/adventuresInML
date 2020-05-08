11d732bb32f448b85cfb54bd88ae79c1ed762a11
Szabolcs Ferenczi <szabi22@gmail.com>
2020-05-08 16:34:35 +0100

    Added model for prediction.

--- deployments/titanic/app.py
+++ deployments/titanic/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from pycaret.classification import *
 import os
 import pandas as pd
@@ -29,5 +29,18 @@ def predict():
     survival = int(prediction.Label[0])
     score = float(prediction.Score[0])
     return render_template('home.html',
-                           pred='Survival prediction {}'.format(survival),
+                           pred=survival,
                            likelihood="{0:.1f}%".format(score * 100))
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
